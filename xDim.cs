3403408e3c8a7c8d6431f3c7d9d918414ad92889
Jack <jack@jackb.me>
2024-10-10 03:01:26 -0400

    Add DPI retrieval and barcode dimension handling
    Updated `MonitorUtilities.cs` to include methods for retrieving the DPI of windows using P/Invoke. Introduced a `DPITypes` enum and necessary `using` directives. Added `MonitorUtilities` static class with `GetWindowDPI` and `GetMainWindowDPI` methods.
    
    Enhanced `xDim.cs` with `XDim` and `Barcodes` classes. `XDim` class includes properties for `Mils`, `DPI`, and calculated properties `Scale` and `PPE`. `Barcodes` class provides methods to generate `XDim` objects and find the closest `XDim` to a given mils value.

---
+++ xDim.cs
@@ -0,0 +1,38 @@
+﻿namespace Zint.CLI
+{
+    public class XDim
+    {
+        public double Mils { get; set; }
+        public double DPI { get; set; }
+        public double Scale => Math.Round(Controller.GetScale(Mils / 1000, DPI), 3);
+        public double PPE => Math.Round(Controller.GetScale(Mils / 1000, DPI) * 2, 3);
+    }
+
+    public class Barcodes
+    {
+        public static List<XDim> GetXDims(int dpi, bool isVector)
+        {
+            var ret = new List<XDim>();
+            var scaleInc = isVector ? 0.1 : 0.5;
+
+            for (var i = scaleInc; ; i += scaleInc)
+            {
+                var mil = Math.Round(Controller.GetMils(i, dpi) * 1000, 3);
+
+                if (mil > 100)
+                    break;
+
+                ret.Add(new XDim() { Mils = mil, DPI = dpi });
+            }
+            return ret;
+        }
+
+        public static XDim GetClosestXDim(int dpi, double mils, bool isVector = false)
+        {
+            var xDims = GetXDims(dpi, isVector);
+            var closest = xDims.OrderBy(x => Math.Abs(x.Mils - mils)).First();
+            return closest;
+        }
+
+    }
+}
